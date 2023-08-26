0bec3134473eca534e99f5a8bdd8db370d914f59
hopeinjavascript <hopeinjavascript@gmail.com>
2023-08-26 15:31:21 +0530

    feat: add delete mutation on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useMutation } from '@apollo/client';
 
-const query = gql`
-  query getProducts {
+const GET_PRODUCTS = gql`
+  query GetProducts {
     products {
+      id
+      title
+      colors
+      sizes
+    }
+  }
+`;
+const DELETE_PRODUCT = gql`
+  mutation DeleteProduct($id: ID!) {
+    deleteProduct(id: $id) {
       id
       title
       colors
@@ -13,17 +23,60 @@ const query = gql`
 `;
 
 function App() {
-  const { data, loading, error } = useQuery(query);
-  console.log({ data, loading, error });
+  // const client = useApolloClient();
+
+  const { data, loading, error } = useQuery(GET_PRODUCTS);
+  const [removeProduct] = useMutation(DELETE_PRODUCT);
+
+  const handleDeleteProduct = (productId) => {
+    removeProduct({
+      variables: { id: productId },
+      update: (cache) => {
+        const newProducts = data.products.filter(
+          (product) => product.id !== productId
+        );
+        cache.writeQuery({
+          query: GET_PRODUCTS,
+          data: { products: newProducts },
+        });
+      },
+      onCompleted: (data) => console.log({ onCompleted: data }),
+      onError: (error) => console.error({ onError: error }),
+    });
+    // client.mutate({
+    //   mutation: DELETE_PRODUCT,
+    //   variables: { id: productId },
+    //   update: (cache) => {
+    //     const newProducts = data.products.filter(
+    //       (product) => product.id !== productId
+    //     );
+    //     cache.writeQuery({
+    //       query: GET_PRODUCTS,
+    //       data: { products: newProducts },
+    //     });
+    //   },
+    // });
+  };
 
   if (loading) return 'Loading...';
 
   return (
     <div className="App">
       <h2>Products</h2>
-      {data?.products.map((product) => (
-        <p key={product.id}>{product.title}</p>
-      ))}
+
+      <div className="product-list">
+        {data?.products.map((product) => (
+          <p key={product.id}>
+            <span>{product.title}</span>
+            <button
+              type="button"
+              onClick={() => handleDeleteProduct(product.id)}
+            >
+              Delete
+            </button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
